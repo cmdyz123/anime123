c38be972cf5e56733a47a9895a7a216163b219b5
cmdyz123 <1414646277@qq.com>
2025-12-29 11:33:31 +0800

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/anime123/' // 替换为你的GitHub仓库名称
-})
